b99c892271298417827746813ecadbbd5f1dd3fb
Leonid Gorunov <leonid.g@siteground.com>
2021-07-05 17:56:25 +0300

    add a new logic for the home page title

--- functions.php
+++ functions.php
@@ -13,82 +13,74 @@
 
 // This theme requires WordPress 5.3 or later.
 
-add_filter( 'wp_title', 'adjust_wp_title' );
-
-
-function adjust_wp_title( $title )
-{
-  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    $title = __( 'Custom title for home page' );
-  }
-  return $title;
-}
-
-function add_custom_post_type() {
-		register_post_type( 'training_cpt',
-		array(
-	            'labels' => array(
-	                'name' => __( 'Training_cpt' ),
-	                'singular_name' => __( 'Training_cpts' ),
-									'description' => __('Short description of the cpt'),
-									'publicly_queryable' => true,
-									'hierarchical' => true
-	            ),
-	            'public' => true,
-							'show_in_menu' => true,
-							'menu_position' => 2,
-	            'has_archive' => true
-
-
-	        )
-	    );
-		}
 
 
 
-add_action( 'init', 'add_custom_post_type' );
+/*
+* Creating a function to create our CPT
+*/
 
-function wpdocs_create_book_taxonomies() {
-    // Add new taxonomy, make it hierarchical (like categories)
+function wpdocs_kantbtrue_init() {
     $labels = array(
-        'name'              => _x( 'Genres', 'taxonomy general name', 'textdomain' ),
-        'singular_name'     => _x( 'Genre', 'taxonomy singular name', 'textdomain' ),
-        'search_items'      => __( 'Search Genres', 'textdomain' ),
-        'all_items'         => __( 'All Genres', 'textdomain' ),
-        'parent_item'       => __( 'Parent Genre', 'textdomain' ),
-        'parent_item_colon' => __( 'Parent Genre:', 'textdomain' ),
-        'edit_item'         => __( 'Edit Genre', 'textdomain' ),
-        'update_item'       => __( 'Update Genre', 'textdomain' ),
-        'add_new_item'      => __( 'Add New Genre', 'textdomain' ),
-        'new_item_name'     => __( 'New Genre Name', 'textdomain' ),
-        'menu_name'         => __( 'Genre', 'textdomain' ),
+        'name'                  => _x( 'Recipes', 'Post type general name', 'recipe' ),
+        'singular_name'         => _x( 'Recipe', 'Post type singular name', 'recipe' ),
+        'menu_name'             => _x( 'Recipes', 'Admin Menu text', 'recipe' ),
+        'name_admin_bar'        => _x( 'Recipe', 'Add New on Toolbar', 'recipe' ),
+        'add_new'               => __( 'Add New', 'recipe' ),
+        'add_new_item'          => __( 'Add New recipe', 'recipe' ),
+        'new_item'              => __( 'New recipe', 'recipe' ),
+        'edit_item'             => __( 'Edit recipe', 'recipe' ),
+        'view_item'             => __( 'View recipe', 'recipe' ),
+        'all_items'             => __( 'All recipes', 'recipe' ),
+        'search_items'          => __( 'Search recipes', 'recipe' ),
+        'parent_item_colon'     => __( 'Parent recipes:', 'recipe' ),
+        'not_found'             => __( 'No recipes found.', 'recipe' ),
+        'not_found_in_trash'    => __( 'No recipes found in Trash.', 'recipe' ),
+        'featured_image'        => _x( 'Recipe Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'recipe' ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'recipe' ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'recipe' ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'recipe' ),
+        'archives'              => _x( 'Recipe archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'recipe' ),
+        'insert_into_item'      => _x( 'Insert into recipe', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'recipe' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this recipe', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'recipe' ),
+        'filter_items_list'     => _x( 'Filter recipes list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'recipe' ),
+        'items_list_navigation' => _x( 'Recipes list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'recipe' ),
+        'items_list'            => _x( 'Recipes list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'recipe' ),
     );
-
     $args = array(
-        'hierarchical'      => true,
-        'labels'            => $labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'genre' ),
+        'labels'             => $labels,
+        'description'        => 'Recipe custom post type.',
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'recipe' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 20,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail' ),
+        'taxonomies'         => array( 'category', 'post_tag' ),
+        'show_in_rest'       => true
     );
 
-    register_taxonomy( 'genre', array( 'training_cpt' ), $args );
+    register_post_type( 'Recipe', $args );
+}
+add_action( 'init', 'wpdocs_kantbtrue_init' );
 
-    unset( $args );
-    unset( $labels );
-
-		add_filter('pre_get_document_title', 'change_home_page_title');
-	function change_home_page_title($title) {
-			if (is_home()) {
-					return 'My Custom Title';
-			}
-			return $title;
+	if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
+		require get_template_directory() . '/inc/back-compat.php';
 	}
 
-if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
-	require get_template_directory() . '/inc/back-compat.php';
+	add_filter('pre_get_document_title', 'change_home_page_title');
+function change_home_page_title($title) {
+    if (is_home()) {
+        return 'My Custom Title';
+    }
+    return $title;
 }
+
 
 if ( ! function_exists( 'twenty_twenty_one_setup' ) ) {
 	/**
